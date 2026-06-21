7d60bf47c357d3501bab4abe9c503114a1e19c58
Tony Shou <a.mashou04@gmail.com>
2026-06-21 01:17:18 -0400

    Worked on new Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
 export default function Navbar() {
-  const buttonStyle = "px-4 hover:bg-slate-700";
+  const buttonStyle = "px-12 hover:bg-slate-700 text-white";
 
   const links = [
     <>
-      <button className={buttonStyle}>Home</button>
-      <button className={buttonStyle}>About</button>
-      <button className={buttonStyle}>Skills</button>
-      <button className={buttonStyle}>Projects</button>
-      <button className={buttonStyle}>Contact</button>
+      <a className={buttonStyle} href="#home">
+        Home
+      </a>
+      <a className={buttonStyle} href="#about">
+        About
+      </a>
+      <a className={buttonStyle} href="#skills">
+        Skills
+      </a>
+      <a className={buttonStyle} href="#projects">
+        Projects
+      </a>
+      <a className={buttonStyle} href="#contact">
+        Contact
+      </a>
     </>,
   ];
 
   return (
-    <nav className="bg-slate-600 text-white fixed p-4 w-full font-sans justify-between mx-auto">
-      <div className="flex justify-center ml-10 space-x-2">{links}</div>
+    <nav className="font-sans-sora text-2xl bg-sky-950 fixed p-4 w-full justify-between mx-auto">
+      <div className="flex justify-center">{links}</div>
     </nav>
   );
 }
