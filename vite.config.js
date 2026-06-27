e4bd8169131e765f213a6cfb395106b0db2d4b2b
Tony Shou <a.mashou04@gmail.com>
2026-06-27 18:52:41 -0400

    Configure Vite server to use polling for file watching

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwind from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
